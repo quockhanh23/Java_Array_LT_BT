a3dbd797f4f45b4b7fd0f44cd30f01bc3428b3a4
unknown <yeuthuongvoivangdiqua@gmail.com>
2021-10-08 15:30:34 +0700

    alo

---
+++ src/com/company/DaoNguocMang.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DaoNguocMang {
+
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scan = new Scanner(System.in);
+        do {
+            System.out.println("Enter you size");
+            size = scan.nextInt();
+            if (size > 20)
+                System.out.println("size do not exceed 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("enter element" + (i + 1) + ": ");
+            array[i] = scan.nextInt();
+            i++;
+        }
+        System.out.printf("%-20s%s", "Elements in array: ", "");
+        for (int j = 0; j < array.length;j++) {
+            System.out.print(array[j] + "\t");
+        }
+        for (int j = 0; j < array.length / 2;j ++) {
+            int temp = array[j];
+            array[j] = array[size - 1 - j];
+            array[size - 1 - j] = temp;
+        }
+        System.out.printf("\n%-20s%s", "Reverse array: ", "");
+        for (int j  = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+    }
+}
+
+
---
+++ src/com/company/GiaTriLonNhat.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class GiaTriLonNhat {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scan = new Scanner(System.in);
+      do {
+          System.out.println("Nhập tổng số phần tử ");
+          size = scan.nextInt();
+          System.out.println("Số phần tử muốn nhập của bạn là :" + size);
+          if (size > 20) {
+              System.out.println("size nhỏ hơn 20 ");
+          }
+      } while (size > 20);
+      array  = new  int[size];
+        int i = 0;
+        while (i < array.length) {
+
+            System.out.print("Nhập phần tử thứ " + (i + 1) + " : ");
+            array[i] = scan.nextInt();
+            i++;
+
+        }
+        System.out.print("Danh sách phần tử: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j + 1;
+            }
+
+        }
+        System.out.println("Phần tử lớn nhất là : "+ max);
+    }
+    }
+
+
+
+
+
+
